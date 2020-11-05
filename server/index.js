1885a62c7fbf2b7bec37bff6fe370b41a02493c9
Linto Cheeran <linto.cet@gmail.com>
2020-11-05 07:01:43 +0530

    add ssr for app

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from "path";
 
 import "localstorage-polyfill";
 
+import App from "../src/App";
 import Home from "../src/Home";
 // import { getNews } from "../src/modules/actions";
 import createStore from "../src/modules/store";
@@ -24,6 +25,46 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./build"));
 
+app.get("/app", async (req, res) => {
+  console.log(req.path, req.query);
+
+  const store = createStore();
+
+  // await store.dispatch(getNews("*"));
+  // await new Promise((r) => setTimeout(r, 10 * 1000));
+
+  const { articles } = await fetch(
+    `http://newsapi.org/v2/everything?apiKey=${API_KEY}&q=${
+      req.query.q || "*"
+    }&sortBy=popularity`
+  ).then((response) => response.json());
+
+  await store.dispatch({ type: "NEWS_RECEIVED", json: articles || [] });
+  await store.dispatch({ type: "NEWS_SELECTED", newsId: req.query.key || "0" });
+
+  const html = ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <StaticRouter context={{}} location={req.url}>
+        <App />
+      </StaticRouter>
+    </Provider>
+  );
+  // console.log("html", html);
+
+  const reduxState = store.getState();
+  // console.log("reduxState", reduxState);
+
+  const build = fs.readFileSync(path.resolve("./build/index.html"), "utf8");
+  return res.send(
+    build.replace(
+      '<div id="root"></div>',
+      `<div id="root">${html}</div><script>window.REDUX_DATA=${JSON.stringify(
+        reduxState
+      )}</script>`
+    )
+  );
+});
+
 app.get("/*", async (req, res) => {
   console.log(req.path, req.query);
 
@@ -37,6 +78,7 @@ app.get("/*", async (req, res) => {
       req.query.q || "*"
     }&sortBy=popularity`
   ).then((response) => response.json());
+
   await store.dispatch({ type: "NEWS_RECEIVED", json: articles || [] });
 
   const html = ReactDOMServer.renderToString(
